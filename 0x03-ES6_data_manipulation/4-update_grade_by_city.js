3e24778a58940886e78a9004ef051ab440adf9de
Lovette Duke <qutyluv@gmail.com>
2024-08-14 01:31:49 +0100

    Task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,21 @@
+/**
+ * Returns an array of students for a specified city with their new grade
+ */
+
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  if (!Array.isArray(newGrades)) {
+    return [];
+  }
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const [newGrade] = newGrades.filter((item) => item.studentId === student.id);
+      return {
+        ...student,
+        grade: newGrade ? newGrade.grade : 'N/A',
+      };
+    });
+}
